d408541892b709adcd7294312045e7a3bd68bd4c
Johan Ek <ekjoh@student.chalmers.se>
2020-01-30 14:57:55 +0100

    Started chaining the with parts

--- Lab/views.sql
+++ Lab/views.sql
@@ -31,21 +31,24 @@ CREATE OR REPLACE VIEW UnreadMandatory AS(
 	EXCEPT
 	SELECT student, course FROM PassedCourses
 );
-
+/*
 SELECT student,COUNT(course) as mandatoryLeft FROM UnreadMandatory GROUP BY student;
-
-
-
-CREATE OR REPLACE VIEW PathToGraduation ( 
-	WITH 
-		Students.idnr AS student FROM Students
-	LEFT OUTER JOIN
+*/
+CREATE OR REPLACE VIEW PathToGraduation AS( 
 	WITH
 		totalCredits AS (
-			SELECT SUM(credits) FROM PassedCourses GROUP BY student
-			)
+			SELECT student,SUM(credits) AS credits FROM PassedCourses GROUP BY student
+		),
+		mandatoryLeft AS (
+			SELECT student, COUNT(course) AS mandatoryLeft FROM UnreadMandatory GROUP BY student
+		),
+		allStudents AS (
+			SELECT idnr AS student FROM Students
+		)
+	SELECT allStudents.student, COALESCE(totalCredits.credits,0), COALESCE(mandatoryLeft.mandatoryLeft,0) AS mandatoryLeft FROM allStudents	
+	LEFT OUTER JOIN totalCredits USING (student) LEFT OUTER JOIN mandatoryLeft USING (student)
 );
-
+/*
 SELECT student, SUM(credits) FROM PassedCourses GROUP BY student
 
 SELECT student, SUM(credits) 
@@ -61,3 +64,4 @@ GROUP BY FinishedCourses.student;
 SELECT student, COUNT(FinishedCourses.course) 
 FROM FinishedCourses, Classified 
 WHERE grade != 'U' AND FinishedCourses.course = Classified.course AND Classified.classification = 'seminar' GROUP BY FinishedCourses.student;
+*/
