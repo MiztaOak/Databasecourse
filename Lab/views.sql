36bad36543c9cddb4c7e2230e17d7a6b57ce2351
Johan Ek <ekjoh@student.chalmers.se>
2020-01-30 13:11:21 +0100

    Began work on the views

--- Lab/views.sql
+++ Lab/views.sql
@@ -1,3 +1,34 @@
-CREATE VIEW BasicInformation AS(
-	SELECT 
-	
+CREATE OR REPLACE VIEW BasicInformation AS(
+	SELECT Students.idnr, Students.name, Students.login, Students.program,
+	StudentBranches.name AS branch FROM Students,StudentBranches
+	WHERE Students.idnr = StudentBranches.student
+);
+
+CREATE OR REPLACE VIEW FinishedCourses AS(
+	SELECT Taken.student, Taken.course, Taken.grade, Courses.credits
+	FROM Taken, Courses
+	WHERE Taken.course = Courses.code
+);	
+
+CREATE OR REPLACE VIEW PassedCourses AS(
+	SELECT student, course, credits
+	FROM FinishedCourses
+	WHERE grade != 'U'
+);
+
+CREATE OR REPLACE VIEW Registrations AS(
+	(SELECT student, course , 'registered' AS status FROM Registered
+	UNION
+	SELECT student, course , 'waiting' AS status FROM WaitingList)
+	ORDER BY status ASC
+);
+
+CREATE OR REPLACE VIEW UnreadMandatory(
+	(SELECT Student.idnr, MandatoryProgram.course FROM Student, MandatoryProgram
+	WHERE Student.program = MandatoryProgram.program
+	UNION
+	SELECT Student.student, MandatoryBranches.course FROM StudentBranches, MandatoryBranches
+	WHERE StudentBranches.branch = MandatoryBranches.branch)
+	EXCEPT
+	SELECT * FROM PassedCourses
+);
