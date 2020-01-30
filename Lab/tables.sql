b1d2d4a1cc7d5ba2bdb0ab78006f2ddd52005254
Johan Ek <ekjoh@student.chalmers.se>
2020-01-30 09:47:46 +0100

    added slq files

---
+++ Lab/tables.sql
@@ -0,0 +1,102 @@
+CREATE TABLE Students (
+	idnr INT PRIMARY KEY,
+	name TEXT NOT NULL, 
+	login TEXT NOT NULL,
+	program TEXT NOT NULL
+);
+
+CREATE TABLE Branches (
+	name TEXT NOT NULL,
+	program TEXT NOT NULL,
+	PRIMARY KEY(name, program)
+);
+
+CREATE TABLE Courses (
+	code TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	credits FLOAT NOT NULL,
+	department TEXT NOT NULL,
+	CHECK (credits >= 0)
+);
+
+CREATE TABLE LimitedCourses (
+	code TEXT PRIMARY KEY,
+	capacity INT NOT NULL,
+	FOREIGN KEY (code) REFERENCES Courses(code)
+);
+
+CREATE TABLE StudentBranches (
+	student INT PRIMARY KEY,
+	name TEXT NOT NULL,
+	program TEXT NOT NULL,
+	FOREIGN KEY (student) REFERENCES Students(idnr),
+	FOREIGN KEY (name, program) REFERENCES Branches(name,program)
+);
+
+CREATE TABLE Classifications (
+	name TEXT PRIMARY KEY
+);
+
+CREATE TABLE Classified (
+	course TEXT,
+	classification TEXT,
+	PRIMARY KEY (course,classification),
+	FOREIGN KEY (course) REFERENCES Courses(code),
+	FOREIGN KEY (classification) REFERENCES Classifications(name)
+);
+
+CREATE TABLE MandatoryProgram (
+	course TEXT,
+	program TEXT,
+	PRIMARY KEY (course,program),
+	FOREIGN KEY (course) REFERENCES Courses(code)
+);
+
+CREATE TABLE MandatoryBranch (
+	course TEXT,
+	branch TEXT, 
+	program TEXT,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (course) REFERENCES Courses(code),
+	FOREIGN KEY (branch, program) REFERENCES Branches(name, program)
+);
+
+CREATE TABLE RecommendedBranch (
+	course TEXT,
+	branch TEXT,
+	program TEXT,
+	PRIMARY KEY (course,branch,program),
+	FOREIGN KEY (course) REFERENCES Courses(code),
+	FOREIGN KEY (branch, program) REFERENCES Branches(name, program)
+);
+
+CREATE TABLE Registered (
+	student INT,
+	course TEXT,
+
+	PRIMARY KEY (student, course),
+	FOREIGN KEY (student) REFERENCES Students(idnr),
+	FOREIGN KEY (course) REFERENCES Courses(code)
+);
+
+CREATE TABLE Taken (
+	student INT,
+	course TEXT,
+	grade CHAR(1) NOT NULL,
+	CHECK (grade = '3' OR grade = '4' OR grade = '5' OR grade = 'U'),
+
+	PRIMARY KEY (student, course),
+	FOREIGN KEY (student) REFERENCES Students(idnr),
+	FOREIGN KEY (course) REFERENCES Courses(code)
+);
+
+CREATE TABLE WaitingList (
+	student INT,
+	course TEXT,
+	position INT NOT NULL,
+	CHECK (position >= 0),
+
+	PRIMARY KEY (student, course),
+	FOREIGN KEY (student) REFERENCES Students(idnr),
+	FOREIGN KEY (course) REFERENCES LimitedCourses(code)
+);
---
+++ Lab/views.sql
@@ -0,0 +1,3 @@
+CREATE VIEW BasicInformation AS(
+	SELECT 
+	
